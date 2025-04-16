328fa5a12aae51f40af1871381a83621c1b2e551
Boussah <benslimane.bilel24@gmail.com>
2025-04-16 20:36:08 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Boussah – Site vitrine
+
+Site Next.js pour la direction artistique et production musicale de Boussah.
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: '#8B0000' }}>Bienvenue sur Boussah</h1>
+      <p>Site vitrine – Production, Mix, Direction Artistique</p>
+    </div>
+  );
+}
